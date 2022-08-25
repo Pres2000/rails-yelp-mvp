d5ebdbba11e270c880f98535e0f6d3e4d013c815
Preston Hogaboam <pjhogaboam@gmail.com>
2022-08-25 08:26:07 +0200

    need to add review option to each restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/restaurants', to: 'restaurants#index', as: :restaurant
+  get '/restaurants/:id/show', to: 'restaurants#show', as: :show
 
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+
+  # add a new review to a restaurant
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+
+  get '/restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post '/restaurants/:restaurant_id/reviews', to: 'reviews#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([{ name: 'IHOP', address: '950 E Madison St', phone_number: '1-800-ihop', category: 'belgian' },
+                   { name: 'WafflesNMore', address: 'Main St', phone_number: '1-800-waffles', category: 'belgian' },
+                   { name: 'Olive Garden', address: 'Division St', phone_number: '1-800-olive', category: 'italian' },
+                   { name: 'Iron Wok', address: 'Palouse River Dr', phone_number: '1-800-wok', category: 'chinese' },
+                   { name: 'Texas Rd House', address: 'Division St', phone_number: '1-800-tex', category: 'japanese' }])
